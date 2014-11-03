bfab5efebce16286d756eb00a0b63a2dd5e2aaee
Dmitry Vlasov <yablokoff@Dmitrys-MacBook-Pro.local>
2014-11-04 02:13:54 +0400

    Added ibooks

---
+++ ibooks/script/all.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    /*- Promo Carousel -*/
+
+    $(".global-carousel").owlCarousel({
+        pagination : false,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+
+    /*- Promo Carousel End -*/
+
+
+});
+
+
