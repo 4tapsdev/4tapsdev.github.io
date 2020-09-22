74ca5cb6579ddec7cd706bdc43297e27e50db141
aly <aly8356@gmail.com>
2020-09-22 18:16:43 +0400

    404

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 export default () => (
-    <div>
-        <h1>404 - Oh no's! We couldn't find that page :(</h1>
-    </div>
+    <Fragment>
+        <Header />
+        <main id="app-main">
+            <h1 className="title-404">404</h1>
+            <p className="plain-text">Oh no's! We couldn't find that page :(</p>
+        </main>
+        <Footer />
+    </Fragment>
 )
